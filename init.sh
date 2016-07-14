8deecbabca270d930aab7b6e2c6569f4b70927ea
Andriy Kyrylenko <andriy.kyrylenko@gmail.com>
2016-07-14 15:05:37 +0300

    Add jenkins_cmd running immediately after init finished

--- init.sh
+++ init.sh
@@ -53,6 +53,61 @@ STAGE_URL="$STAGE_USER@$STAGE_ADDR"
 read -p "Please provide path to your public key: [$HOME/.ssh/id_rsa.pub] " PUBKEY_PATH
 PUBKEY_PATH=${PUBKEY_PATH:-$HOME/.ssh/id_rsa.pub}
 
+# Jenkins parameters
+
+printf "Please provide ruby gemset you are using in your project application.\n"
+printf "To have Jenkins job run correctly you may need to open generated job Configure page, goto Build Environment section. And set checkbox ""Run the build in a RVM-managed environment"". Also it will require to set ruby version@gemset in the Implementation variable below the checkbox.\n"
+read -p "Ruby gemset [myapp]: " RUBY_GEMSET
+RUBY_GEMSET=${RUBY_GEMSET:-myapp}
+printf "Please use ""$RUBY_VERSION@$RUBY_GEMSET"" for the Implementation variable below the checkbox.\n"
+
+DEFAULT_GITHUB_PATH="some_github_user/$PROJECT_NAME.git"
+DEFAULT_GITHUB_PATH="akyrylenko/testgenerator.git" # for development
+
+set -f
+oldifs="$IFS"
+IFS=':'; arrayIN=($REPO_URL)
+IFS="$oldifs"
+DEFAULT_GITHUB_PATH="${arrayIN[1]}"
+echo "$DEFAULT_GITHUB_PATH"
+set +f
+
+printf "Please provide git repository path. Default is taken from the Git url but you can update it.\n"
+read -p "Git repository path: [$DEFAULT_GITHUB_PATH] " GITHUB_PATH
+GITHUB_PATH=${GITHUB_PATH:-$DEFAULT_GITHUB_PATH}
+
+DEFAULT_JENKINS_URL="http://localhost:8080"
+#DEFAULT_JENKINS_URL="http://jenkins.gera-it.com" # for development
+printf "Please provide jenkins address\n"
+read -p "Jenkins Address: [$DEFAULT_JENKINS_URL] " JENKINS_URL
+JENKINS_URL=${JENKINS_URL:-$DEFAULT_JENKINS_URL}
+
+DEFAULT_JENKINS_USER_ID=""
+#DEFAULT_JENKINS_USER_ID="akyrylenko" # for development
+printf "Please provide jenkins UserID. It can be taken from the Jenkins User Profile page\n"
+printf "You can access it by User Dropdown in the top right corner of the Jenkins UI\n"
+read -p "Jenkins UserID: [$DEFAULT_JENKINS_USER_ID] " JENKINS_USER_ID
+JENKINS_USER_ID=${JENKINS_USER_ID:-$DEFAULT_JENKINS_USER_ID}
+
+DEFAULT_JENKINS_API_TOKEN=""
+#DEFAULT_JENKINS_API_TOKEN="ab5495cf6b05e88a66b2c9fd6eb363d1" # for development
+printf "Please provide jenkins API Token. It is required if you have setup security access to the CI server. It can be taken from the jenkins web UI\n"
+read -p "Please provide jenkins API Token: [$DEFAULT_JENKINS_API_TOKEN] " JENKINS_API_TOKEN
+JENKINS_API_TOKEN=${JENKINS_API_TOKEN:-$DEFAULT_JENKINS_API_TOKEN}
+
+DEFAULT_USER_FULLNAME="Firstname Lastname"
+DEFAULT_USER_FULLNAME="Andriy Kyrylenko" # for development
+DEFAULT_USER_FULLNAME="Jenkins User" # for development
+printf "Please provide user fullname for github identifiing in jenkins run.\n"
+read -p "Git user fullname: [$DEFAULT_USER_FULLNAME] " USERNAME
+USER_FULLNAME=${USER_FULLNAME:-$DEFAULT_USER_FULLNAME}
+
+DEFAULT_USER_EMAIL="user@example.com"
+DEFAULT_USER_EMAIL="andriy.kyrylenko@gmail.com" # for development
+DEFAULT_USER_EMAIL="jenkins@jenkins.local" # for development
+read -p "Please provide user email for github identifiing in jenkins run: [$DEFAULT_USER_EMAIL] " USER_EMAIL
+USER_EMAIL=${USER_EMAIL:-$DEFAULT_USER_EMAIL}
+
 #
 # #######################
 # # Prerequisites start #
@@ -191,3 +246,11 @@ git push -u origin master
 cd $CURRENT_PATH
 printf "\n"
 printf "Note: Please configure your staging smtp getaway and other staging vars in $CURRENT_PATH/provisioning/group_vars/staging [Ansible] \n"
+
+
+printf "Installing jenkins_api_client...\n"
+gem install jenkins_api_client --no-ri --no-rdoc
+
+# Script that should be run from rpsk project folder
+
+ruby ./jenkins_cmd.rb --project-name $PROJECT_NAME --jenkins-url $JENKINS_URL --jenkins-user-id $JENKINS_USER_ID --jenkins-api-token $JENKINS_API_TOKEN --ruby-version $RUBY_VERSION --ruby-gemset $RUBY_GEMSET --github-url $REPO_URL --github-path $GITHUB_PATH --user-fullname $USER_FULLNAME --user-email $USER_EMAIL
